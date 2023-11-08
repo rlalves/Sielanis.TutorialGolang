89cd0ab7b6e40d9e0b7c857aa45fbf81e7b24118
Raazak <rlalves4880@gmail.com>
2023-11-08 07:06:22 -0300

    uso de função random

--- Exercicio-02/Cumprimentos/cumprimentos.go
+++ Exercicio-02/Cumprimentos/cumprimentos.go
@@ -3,6 +3,7 @@ package Cumprimentos
 import (
 	"fmt"
 	"errors"
+	"math/rand"
 )
 
 func Ola(nome string) (string, error){
@@ -11,6 +12,21 @@ func Ola(nome string) (string, error){
 	}
 
 	var mensagem string
-	mensagem = fmt.Sprintf("Olá, %v. Bem vindo!", nome)
+	mensagem = fmt.Sprintf(FormatosDeOla(), nome)
 	return mensagem, nil
+}
+
+func FormatosDeOla() string {
+	formatos := []string{
+		"Olá, %v. Bem vindo!",
+		"Olá, %v. Bem vindo! Hoje é um dia lindo!",
+		"Olá, %v. Bem vindo! Hoje está um dia lindo!",
+		"Olá, %v. Bem vindo! Hoje está um dia lindo e ensolarado!",
+		"Olá, %v. Bem vindo! Hoje está um dia lindo e ensolarado! Aproveite!",
+		"Olá, %v. Bem vindo! Hoje está um dia lindo e ensolarado! Aproveite muito!",
+		"Olá, %v. Bem vindo! Hoje está um dia lindo e ensolarado! Aproveite muito a vida!",
+		"Olá, %v. Bem vindo! Hoje está um dia lindo e ensolarado! Aproveite muito a vida e a natureza!",
+	}
+
+	return formatos[rand.Intn(len(formatos))]
 }
